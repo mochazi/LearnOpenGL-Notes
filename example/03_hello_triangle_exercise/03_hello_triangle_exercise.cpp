d8c30e31942c459037b5ef206dcbdedcde34156d
mochazi <mochazi888@gmail.com>
2023-06-17 21:43:06 +0800

    03_hello_triangle_exercise、04_shader_glsl、05_shader_class🚀

---
+++ example/03_hello_triangle_exercise/03_hello_triangle_exercise.cpp
@@ -0,0 +1,14 @@
+#include "03_hello_triangle_exercise.hpp"
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
---
+++ example/03_hello_triangle_exercise/03_hello_triangle_exercise.hpp
@@ -0,0 +1,11 @@
+#ifndef EXAMPLE_02_H
+#define EXAMPLE_02_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
+#endif //EXAMPLE_02_H
