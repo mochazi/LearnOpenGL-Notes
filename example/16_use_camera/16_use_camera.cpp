4d7de3e9dcecfaa881bc0f18754c2b62a5dcde80
mochazi <mochazi888@gmail.com>
2023-07-12 18:09:47 +0800

    16_use_camera

---
+++ example/16_use_camera/16_use_camera.cpp
@@ -0,0 +1,74 @@
+#include "16_use_camera.hpp"
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+
+    // 相机按键控制
+    float cameraSpeed = 2.5f * deltaTime;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        cameraPos += cameraSpeed * cameraFront;
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        cameraPos -= cameraSpeed * cameraFront;
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    }
+}
+
+// 鼠标移动监听
+void mouse_callback(GLFWwindow *window, double xpos, double ypos)
+{
+    // cout << "xpos " << xpos << endl;
+    // cout << "ypos " << ypos << endl;
+}
+void mouse_button_calback(GLFWwindow *window, int button, int action, int mods)
+{
+
+    if (action == GLFW_PRESS)
+        switch (button)
+        {
+            case GLFW_MOUSE_BUTTON_LEFT:
+                // cout << "mouse left" << endl;
+                break;
+            case GLFW_MOUSE_BUTTON_MIDDLE:
+                // cout << "mouse middle" << endl;
+                break;
+            case GLFW_MOUSE_BUTTON_RIGHT:
+                // cout << "mouse right" << endl;
+                break;
+        }
+}
+
+void cursor_position_callback(GLFWwindow *window, double x, double y)
+{
+    float xpos = float((x - SCREEN_WIDTH / 2) / SCREEN_WIDTH) * 2;
+    float ypos = float(0 - (y - SCREEN_HEIGHT / 2) / SCREEN_HEIGHT) * 2;
+
+    // cout << "xpos " << xpos << endl;
+    // cout << "ypos " << ypos << endl;
+    return;
+}
+
+void scroll_callback(GLFWwindow *window, double x, double y)
+{
+    // cout << "x " << x << endl;
+    // cout << "y " << y << endl;
+    return;
+}
---
+++ example/16_use_camera/16_use_camera.hpp
@@ -0,0 +1,31 @@
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <tool/shader.h>
+#include <geometry/BoxGeometry.h>
+#include <geometry/PlaneGeometry.h>
+#include <geometry/SphereGeometry.h>
+#include <tool/gui.h>
+#include <iostream>
+
+extern int SCREEN_WIDTH;
+extern int SCREEN_HEIGHT;
+
+// camera value
+extern glm::vec3 cameraPos;
+extern glm::vec3 cameraFront;
+extern glm::vec3 cameraUp;
+
+// delta time
+extern float deltaTime;
+extern float lastTime;
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
+void mouse_button_calback(GLFWwindow *window, int button, int action, int mods);
+void scroll_callback(GLFWwindow *window, double x, double y);
+
+#endif //EXAMPLE_H
