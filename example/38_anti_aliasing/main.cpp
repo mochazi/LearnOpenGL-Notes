ac0aed73ff367317648f0e9bed61b688798f175d
mochazi <mochazi888@gmail.com>
2023-07-20 12:30:19 +0800

    38_anti_aliasing

---
+++ example/38_anti_aliasing/main.cpp
@@ -0,0 +1,141 @@
+#include "38_anti_aliasing.hpp"
+
+int SCREEN_WIDTH = 800;
+int SCREEN_HEIGHT = 600;
+
+// camera value
+Camera camera(glm::vec3(0.0f, 0.0f, 5.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+// delta time
+float deltaTime = 0.0f;
+float lastTime = 0.0f;
+
+float lastX = SCREEN_WIDTH / 2.0f; // 鼠标上一帧的位置
+float lastY = SCREEN_HEIGHT / 2.0f;
+
+int main(int argc, char *argv[])
+{
+    glfwInit();
+    // 设置主要和次要版本
+    const char *glsl_version = "#version 330";
+
+    // 片段着色器将作用域每一个采样点（采用4倍抗锯齿，则每个像素有4个片段（四个采样点））
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // 窗口对象
+    GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // -----------------------
+    // 创建imgui上下文
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
+    // 设置样式
+    ImGui::StyleColorsDark();
+    // 设置平台和渲染器
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+    // -----------------------
+
+    // 设置视口
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    glEnable(GL_PROGRAM_POINT_SIZE);
+
+    // 深度测试
+    glEnable(GL_DEPTH_TEST);
+
+    glEnable(GL_MULTISAMPLE); // 多重采样抗锯齿
+
+    // 鼠标键盘事件
+    // 1.注册窗口变化监听
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    // 2.鼠标事件
+    glfwSetCursorPosCallback(window, mouse_callback);
+    // 3.将鼠标隐藏
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    Shader sceneShader("../shader/scene_vert.glsl", "../shader/scene_frag.glsl");
+
+    PlaneGeometry planeGeometry(1.0, 1.0);          // 面板
+    BoxGeometry boxGeometry(1.0, 1.0, 1.0);         // 盒子
+    SphereGeometry sphereGeometry(1.0, 10.0, 10.0); // 圆球
+
+    float fov = 45.0f;                                                          // 视锥体的角度
+    ImVec4 clear_color = ImVec4(25.0 / 255.0, 25.0 / 255.0, 25.0 / 255.0, 1.0); // 25, 25, 25
+
+    unsigned int map = loadTexture("../images/texture/uv_grid_directx.jpg");
+
+    float factor = 0.0;
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastTime;
+        lastTime = currentFrame;
+
+        // 在标题中显示帧率信息
+        // *************************************************************************
+        int fps_value = (int)round(ImGui::GetIO().Framerate);
+        int ms_value = (int)round(1000.0f / ImGui::GetIO().Framerate);
+
+        std::string FPS = std::to_string(fps_value);
+        std::string ms = std::to_string(ms_value);
+        std::string newTitle = "LearnOpenGL - " + ms + " ms/frame " + FPS;
+        glfwSetWindowTitle(window, newTitle.c_str());
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        factor = glfwGetTime();
+        // *************************************************************************
+
+        // 渲染指令
+        // ...
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glm::mat4 view = camera.GetViewMatrix();
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 model = glm::mat4(1.0f);
+
+        sceneShader.use();
+        sceneShader.setMat4("projection", projection);
+        sceneShader.setMat4("view", view);
+        sceneShader.setMat4("model", model);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, map);
+        glBindVertexArray(boxGeometry.VAO);
+        glDrawElements(GL_TRIANGLES, boxGeometry.indices.size(), GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        // 渲染 gui
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
