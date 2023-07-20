c34192cdb8eeb917c329849fdd5b1f12d16d5f39
mochazi <mochazi888@gmail.com>
2023-07-20 11:52:30 +0800

    37_instancing_rock

---
+++ example/37_instancing_rock/main.cpp
@@ -0,0 +1,216 @@
+#include "37_instancing_rock.hpp"
+
+int SCREEN_WIDTH = 800;
+int SCREEN_HEIGHT = 600;
+
+// camera value
+Camera camera(glm::vec3(0.0f, 0.0f, 15.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+// delta time
+float deltaTime = 0.0f;
+float lastTime = 0.0f;
+
+float lastX = SCREEN_WIDTH / 2.0f; // 鼠标上一帧的位置
+float lastY = SCREEN_HEIGHT / 2.0f;
+
+int main(int argc, char *argv[])
+{
+    glfwInit();
+    // 设置主要和次要版本
+    const char *glsl_version = "#version 330";
+
+    // 片段着色器将作用域每一个采样点（采用4倍抗锯齿，则每个像素有4个片段（四个采样点））
+    // glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // 窗口对象
+    GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // -----------------------
+    // 创建imgui上下文
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
+    // 设置样式
+    ImGui::StyleColorsDark();
+    // 设置平台和渲染器
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+    // -----------------------
+
+    // 设置视口
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    glEnable(GL_PROGRAM_POINT_SIZE);
+
+    // 深度测试
+    glEnable(GL_DEPTH_TEST);
+
+    // 鼠标键盘事件
+    // 1.注册窗口变化监听
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    // 2.鼠标事件
+    glfwSetCursorPosCallback(window, mouse_callback);
+    // 3.将鼠标隐藏
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    Shader sceneShader("../shader/scene_vert.glsl", "../shader/scene_frag.glsl");
+    Shader instanceShader("../shader/instance_vert.glsl", "../shader/scene_frag.glsl");
+
+    PlaneGeometry planeGeometry(0.1, 0.1);          // 面板
+    BoxGeometry boxGeometry(0.1, 0.1, 0.1);         // 盒子
+    SphereGeometry sphereGeometry(0.1, 10.0, 10.0); // 圆球
+
+    float fov = 45.0f;                                                          // 视锥体的角度
+    ImVec4 clear_color = ImVec4(25.0 / 255.0, 25.0 / 255.0, 25.0 / 255.0, 1.0); // 25, 25, 25
+
+    Model rock("../images/model/rock/rock.obj");
+    Model planet("../images/model/planet/planet.obj");
+
+    unsigned int amount = 100000;
+    glm::mat4 *modelMatrices;
+    modelMatrices = new glm::mat4[amount];
+    srand(glfwGetTime()); // initialize random seed
+    float radius = 20.0;
+    float offset = 1.5f;
+    for (unsigned int i = 0; i < amount; i++)
+    {
+        glm::mat4 model = glm::mat4(1.0f);
+        // 1. translation: displace along circle with 'radius' in range [-offset, offset]
+        float angle = (float)i / (float)amount * 360.0f;
+        float displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
+        float x = sin(angle) * radius + displacement;
+        displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
+        float y = displacement * 0.4f; // keep height of asteroid field smaller compared to width of x and z
+        displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
+        float z = cos(angle) * radius + displacement;
+        model = glm::translate(model, glm::vec3(x, y, z));
+
+        // 2. scale: Scale between 0.05 and 0.25f
+        float scale = (rand() % 20) / 1000.0f + 0.001;
+        model = glm::scale(model, glm::vec3(scale));
+
+        // 3. rotation: add random rotation around a (semi)randomly picked rotation axis vector
+        float rotAngle = (rand() % 360);
+        model = glm::rotate(model, rotAngle, glm::vec3(0.4f, 0.6f, 0.8f));
+
+        // 4. now add to list of matrices
+        modelMatrices[i] = model;
+    }
+
+    // 设置实例化数组
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, amount * sizeof(glm::mat4), &modelMatrices[0], GL_STATIC_DRAW);
+    for (unsigned int i = 0; i < rock.meshes.size(); i++)
+    {
+        unsigned int VAO = rock.meshes[i].VAO;
+        glBindVertexArray(VAO);
+        // 顶点属性
+        GLsizei vec4Size = sizeof(glm::vec4);
+        glEnableVertexAttribArray(3);
+        glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void *)0);
+        glEnableVertexAttribArray(4);
+        glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void *)(vec4Size));
+        glEnableVertexAttribArray(5);
+        glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void *)(2 * vec4Size));
+        glEnableVertexAttribArray(6);
+        glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void *)(3 * vec4Size));
+
+        glVertexAttribDivisor(3, 1);
+        glVertexAttribDivisor(4, 1);
+        glVertexAttribDivisor(5, 1);
+        glVertexAttribDivisor(6, 1);
+
+        glBindVertexArray(0);
+    }
+
+    float factor = 0.0;
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastTime;
+        lastTime = currentFrame;
+
+        // 在标题中显示帧率信息
+        // *************************************************************************
+        int fps_value = (int)round(ImGui::GetIO().Framerate);
+        int ms_value = (int)round(1000.0f / ImGui::GetIO().Framerate);
+
+        std::string FPS = std::to_string(fps_value);
+        std::string ms = std::to_string(ms_value);
+        std::string newTitle = "LearnOpenGL - " + ms + " ms/frame " + FPS;
+        glfwSetWindowTitle(window, newTitle.c_str());
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        factor = glfwGetTime();
+        // *************************************************************************
+
+        // 渲染指令
+        // ...
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glm::mat4 view = camera.GetViewMatrix();
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 model = glm::mat4(1.0f);
+
+        sceneShader.use();
+        sceneShader.setMat4("projection", projection);
+        sceneShader.setMat4("view", view);
+        model = glm::translate(model, glm::vec3(0.0f, -1.0f, 4.0f));
+        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+        sceneShader.setMat4("model", model);
+
+        planet.Draw(sceneShader);
+
+        // for (unsigned int i = 0; i < amount; i++)
+        // {
+        //   sceneShader.setMat4("model", modelMatrices[i]);
+        //   rock.Draw(sceneShader);
+        // }
+
+        instanceShader.use();
+        instanceShader.setMat4("projection", projection);
+        instanceShader.setMat4("view", view);
+        instanceShader.setInt("diffuseTexture", 0);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, rock.textures_loaded[0].id);
+        for (unsigned int i = 0; i < rock.meshes.size(); i++)
+        {
+            glBindVertexArray(rock.meshes[i].VAO);
+            glDrawElementsInstanced(GL_TRIANGLES, rock.meshes[i].indices.size(), GL_UNSIGNED_INT, 0, amount);
+        }
+
+        // 渲染 gui
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
