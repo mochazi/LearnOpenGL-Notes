ec520e5888fdba072c2876cc44aa4662a958613c
mochazi <mochazi888@gmail.com>
2023-07-20 09:41:12 +0800

    30_blending

---
+++ example/30_blending/30_blending.hpp
@@ -0,0 +1,36 @@
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <tool/shader.h>
+#include <tool/camera.h>
+#include <geometry/BoxGeometry.h>
+#include <geometry/PlaneGeometry.h>
+#include <geometry/SphereGeometry.h>
+#include <tool/gui.h>
+#include <iostream>
+#include "model.h"
+
+extern int SCREEN_WIDTH;
+extern int SCREEN_HEIGHT;
+
+// camera value
+extern Camera camera;
+
+// delta time
+extern float deltaTime;
+extern float lastTime;
+
+// 鼠标上一帧的位置
+extern float lastX;
+extern float lastY;
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
+void mouse_button_calback(GLFWwindow *window, int button, int action, int mods);
+void scroll_callback(GLFWwindow *window, double x, double y);
+unsigned int loadTexture(char const *path);
+
+#endif //EXAMPLE_H
